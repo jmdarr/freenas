508f6eb2eed9e56abc35668c79dcecc37b58c1e0
jmdarr <jeremy.darr@gmail.com>
2017-10-01 20:23:45 -0700

    Initial commit

---
+++ scripts/jails/openvpn_install.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+portsnap fetch
+portsnap extract
+cd /usr/ports/security/openvpn
+make install clean
+mkdir -p /usr/local/etc/openvpn
+echo -e 'openvpn_enable=”YES”\nopenvpn_configfile=”/usr/local/etc/openvpn/openvpn.conf”' >> /etc/rc.conf
+echo -e 'firewall_enable="YES"\nfirewall_script="/etc/ipfw.rules"' >> /etc/rc.conf
+env ASSUME_ALWAYS_YES=YES pkg install geoip
