73591e9ec10c46d531f5a70e70e76580f4350c8d
jdarr <jdarr@bastion.solarsquid.local>
2017-10-01 20:09:58 -0700

    Variabalizing

--- scripts/check_openvpn_all.sh
+++ scripts/check_openvpn_all.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+jail_search_string="transmission"
+
 while read id name; do
     echo "${name}"
     echo "-------------------------------"
     jexec ${id} "/root/check_openvpn.sh"
     echo "-------------------------------"
-done < <(jls | grep transmission | awk '{ print $1" "$3 }')
+done < <(jls | grep "${jail_search_string}" | awk '{ print $1" "$3 }')
