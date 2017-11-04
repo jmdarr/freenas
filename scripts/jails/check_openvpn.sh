6227deb285a5d2245f997ce83adbf551e710cf11
Jeremy Darr <jeremy.darr@gmail.com>
2017-11-04 14:57:07 -0700

    Update check_openvpn.sh

--- scripts/jails/check_openvpn.sh
+++ scripts/jails/check_openvpn.sh
@@ -12,6 +12,33 @@ wantstring='CA, Canada'
 echo -n "Updating geoip database... "
 /usr/local/bin/geoipupdate.sh >/dev/null 2>&1 && echo "success" || echo "failure"
 
+# before we go further, lets make sure our fw rules are for the right interface
+# also try to overcome some VPN shenanigans
+echo -n "Checking for multiple openvpn pids... "
+[ $(ps aux | grep -v grep | grep openvpn | wc -l | awk '{ print $1 }') -gt 1 ] && {
+    echo "found"
+    echo -n "Trying to correct multiple running pids... "
+    until [ $(ps aux | grep -v grep | grep openvpn | wc -l | awk '{ print $1 }') -eq 1 ]; do
+        /usr/sbin/service openvpn stop >/dev/null 2>&1
+        pkill openvpn >/dev/null 2>&1
+        sleep 1
+    done
+    /usr/sbin/service openvpn start >/dev/null 2>&1 && echo "success" || echo "failure"
+} || { echo "not found"; }
+
+echo -n "Checking VPN tunnel iface name against firewall rules... "
+fwiface=$(grep -Eio 'tun[0-9]{1}' /etc/ipfw.rules)
+realiface=$(/sbin/ifconfig | grep -Eio 'tun[0-9]{1}')
+[ "${fwiface}" != "${realiface}" ] && {
+    echo "mismatch"
+    echo -n "Attempting to correct firewall rules... "
+    sed -i '' "s#${fwiface}#${realiface}#g" /etc/ipfw.rules >/dev/null 2>&1
+    /usr/sbin/service ipfw restart >/dev/null 2>&1
+    fwiface=$(grep -Eio 'tun[0-9]{1}' /etc/ipfw.rules)
+    realiface=$(/sbin/ifconfig | grep -Eio 'tun[0-9]{1}')
+    [ "${fwiface}" != "${realiface}" ] && echo "failure" || echo "success"
+} || { echo "match"; }
+
 # Get our output and make sure we're on the VPN
 echo -n "Checking to see if VPN is active... "
 output=$(/usr/local/bin/geoiplookup $(/usr/local/bin/wget -qO - http://icanhazip.com))
